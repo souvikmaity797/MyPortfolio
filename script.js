67480130a56e53c0285547bf5644ba951a2dcea3
souvikmaity797 <121762736+souvikmaity797@users.noreply.github.com>
2024-03-17 04:58:18 +0530

    js added

---
+++ script.js
@@ -0,0 +1,39 @@
+let menuIcon=document.querySelector('#menu-icon');
+let navbar=document.querySelector('.navbar');
+let sections=document.querySelector('section');
+let navLinks=document.querySelector('header nav a');
+window.onscroll=()=>{
+    sections.forEach(sec=>{
+        let top=window.scrollY;
+        let offset=sec.offsetTop-150;
+        let height=sec.offsetHeight;
+        let id=sec.getAttribute('id');
+
+        if(top>=offset && top<offset+height){
+            navLinks.forEach(links=>{
+                links.classList.remove('active');
+                document.querySelector('header nav a [href*=' + id + ' ]').classList.add('active')
+            })
+        }
+    })
+}
+menuIcon.onclick=()=>{
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+ 
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzA67KkppbJdLZ7fub1QpYJoCfQLuvPx2nFAFv45igUoMtcwLGWgzBpCyBz06VTyvnS/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg")
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+        msg.innerHTML ="Message sent successfully!"
+        setTimeout(function(){
+            msg.innerHTML=""
+        },5000)
+        form.reset()
+    })
+    .catch(error => console.error('Error!', error.message))
+})
